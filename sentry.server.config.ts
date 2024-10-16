a79e4627358b0fbf592708faf52ca78a549040e0
DayanAdil17 <89575041+DayanAdil17@users.noreply.github.com>
2024-10-16 22:33:08 +0700

    update sentry configuration

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,6 +6,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://792d4056729aac989575cbeabd7f955b@o4507983709208576.ingest.us.sentry.io/4507983714123776",
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || 'production',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
